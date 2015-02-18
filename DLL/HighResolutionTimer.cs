2e5181278f34ca2fcab02ac6e85434efc062319c
BuildTools <unconfigured@null.spigotmc.org>
2015-02-18 13:22:19 +0100

    check code

--- DLL/HighResolutionTimer.cs
+++ DLL/HighResolutionTimer.cs
@@ -1,38 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace ADDLL
-{    
+namespace DLL
+{
     public enum TimeResolution
     {
-        Seconds         = 1,
-        Milliseconds    = 1000,
-        Microseconds    = 1000000,
-        Nanoseconds     = 1000000000
+        Seconds = 1,
+        Milliseconds = 1000,
+        Microseconds = 1000000,
+        Nanoseconds = 1000000000
     }
 
     public class HighResolutionTimer
     {
+        #region Constructor
+
+        public HighResolutionTimer(bool useDLL)
+        {
+            TimestampStart = 0;
+            TimestampEnd = 0;
+
+            this.useDLL = useDLL;
+        }
+
+        #endregion
+
         #region QueryPerformance DLL Import
-        [DllImport("kernel32.dll")]
-        extern static short QueryPerformanceCounter(ref long x);
 
         [DllImport("kernel32.dll")]
-        extern static short QueryPerformanceFrequency(ref long x);
+        private static extern short QueryPerformanceCounter(ref long x);
+
+        [DllImport("kernel32.dll")]
+        private static extern short QueryPerformanceFrequency(ref long x);
+
         #endregion
 
         #region Variables
+
         private long timestampStart;
         private long timestampEnd;
 
-        private bool useDLL;
+        private readonly bool useDLL;
+
         #endregion
 
         #region Properties
+
         public long TimestampStart
         {
             get { return timestampStart; }
@@ -44,19 +57,11 @@ namespace ADDLL
             get { return timestampEnd; }
             private set { timestampEnd = value; }
         }
-        #endregion
 
-        #region Constructor
-        public HighResolutionTimer(bool useDLL)
-        {
-            TimestampStart = 0;
-            TimestampEnd = 0;
-            
-            this.useDLL = useDLL;
-        }
         #endregion
 
         #region Public Methods
+
         public void Start()
         {
             if (useDLL)
@@ -88,13 +93,11 @@ namespace ADDLL
                 long frequency = 0;
                 QueryPerformanceFrequency(ref frequency);
 
-                return ((timestampEnd - timestampStart) * ((double)timeResolution / frequency));
+                return ((timestampEnd - timestampStart)*((double) timeResolution/frequency));
             }
-            else
-            {
-                return ((timestampEnd - timestampStart) * ((double)timeResolution / Stopwatch.Frequency));
-            }
+            return ((timestampEnd - timestampStart)*((double) timeResolution/Stopwatch.Frequency));
         }
+
         #endregion
     }
 }
