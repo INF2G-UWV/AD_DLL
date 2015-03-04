28c17fb123097094d49fcd6de8ae8d01b3a22b1e
Xingo123 <shinchanwoo@live.nl>
2015-03-04 09:55:43 +0100

    CollectionBase added

---
+++ DLL/CollectionBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public class CollectionBase<T>
+    {
+        private CollectionBase<T> collection = new CollectionBase<T>();
+
+        public void Add(T item)
+        {
+            collection.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            collection.Remove(item);
+        }
+
+        private new void Clear()
+        {
+            collection.Clear();
+        }
+
+        public new int Count()
+        {
+            return collection.Count();
+        }
+    }
+}
