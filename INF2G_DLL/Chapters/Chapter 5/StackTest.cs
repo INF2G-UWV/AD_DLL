1296b45dc9bbdaf3e40af5bc1a9c8a956cd23574
Xingo123 <shinchanwoo@live.nl>
2015-03-17 16:14:09 +0100

    Stack class

---
+++ INF2G_DLL/Chapters/Chapter 5/StackTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLL;
+
+namespace INF2G_DLL.Chapters.Chapter_5
+{
+    internal class StackTest
+    {
+        public static void Run(string[] args)
+        {
+            CStack<string> stack = new CStack<string>(4);
+            stack.Push("test1");
+            stack.Push("test2");
+            stack.Push("test3");
+            stack.Push("test4"); 
+            stack.Push("test5");
+
+            Console.WriteLine(stack.Peek());
+            stack.GetAllStackItems();
+            Console.ReadLine();
+        }
+    }
+}
