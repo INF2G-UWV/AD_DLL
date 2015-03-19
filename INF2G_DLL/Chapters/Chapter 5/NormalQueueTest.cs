f5ae13081e945381732526543abc54eeb47566d6
slmii <cimbom1992.>
2015-03-19 18:00:14 +0100

    Clean up code

--- INF2G_DLL/Chapters/Chapter 5/NormalQueueTest.cs
+++ INF2G_DLL/Chapters/Chapter 5/NormalQueueTest.cs
@@ -9,20 +9,23 @@ namespace INF2G_DLL.Chapters.Chapter_5
     {
         public static void Run(string[] args)
         {
+            // Deze is met tijdmeetingen, zonder UI
+            // QueueTest();
+            
+            // Zonder tijdmeetingen, met UI
             QueueTest2();
         }
 
         public static void QueueTest2()
         {
-            // Create a Generic Queue and Timer;
+            // Create a Generic Queue
             NormalQueue<string> rndWords = new NormalQueue<string>();
-            HighResolutionTimer timer = new HighResolutionTimer(true);
 
             // Two variables which choice stores the command pressed, and value typed in the input;
             string choice, value;
 
             // Commands that are available;
-            Console.WriteLine("\nCommand to follow!");
+            Console.WriteLine("\nCommands to follow!");
             Console.WriteLine("(1) Enqueue");
             Console.WriteLine("(2) Dequeue");
             Console.WriteLine("(3) Peek");
@@ -96,7 +99,7 @@ namespace INF2G_DLL.Chapters.Chapter_5
         public static void QueueTest()
         {
             // Create a Generic Queue and Timer
-            System.Collections.Generic.Queue<int> rndWords = new System.Collections.Generic.Queue<int>();
+            NormalQueue<int> rndWords = new NormalQueue<int>();
             HighResolutionTimer timer = new HighResolutionTimer(true);
 
             // Timer start
@@ -105,7 +108,6 @@ namespace INF2G_DLL.Chapters.Chapter_5
             // Vars to add in the Queue
             int one = 1, three = 3, four = 4, five = 5, two = 2;
 
-            Console.WriteLine("Starting Queue:");
             Console.WriteLine("Adding " + one + ", " + three + ", " + four + ", " + five + ", " + two + " to the queue");
             rndWords.Enqueue(one);
             rndWords.Enqueue(three);
@@ -113,53 +115,49 @@ namespace INF2G_DLL.Chapters.Chapter_5
             rndWords.Enqueue(five);
             rndWords.Enqueue(two);
 
-            // Print the total Queue's in the list
-            PrintValues(rndWords);
-
-            //DeQueue's the first one in the list;
-            Console.WriteLine("\n\nDequeuing '{0}'", rndWords.Dequeue());
-
-            //Looks at the next item in line after the one that has been Dequeued;
-            Console.WriteLine("Peek at next item to Dequeue: {0}", rndWords.Peek());
-
-            //DeQueue's the first one in the list;
-            Console.WriteLine("Dequeuing '{0}'", rndWords.Dequeue());
-
-            //Looks at the next item in line after the one that has been Dequeued;
-            Console.WriteLine("Peek at next item to Dequeue: {0}", rndWords.Peek());
-
-            //DeQueue's the first one in the list;
-            Console.WriteLine("Dequeuing '{0}'", rndWords.Dequeue());
-
-            // Print the total Queue's in the list
-            PrintValues(rndWords);
             Console.WriteLine();
+            // Print the total Queue's in the list
+            rndWords.GetAllQueueItems();
 
-            // Check for if the items are still in the Queue are not
-            Console.WriteLine("\nCheck for number 4 in the list: " + rndWords.Contains(four));
-            Console.WriteLine("Check for number 5 in the list: " + rndWords.Contains(five));
-            Console.WriteLine("Check for number 2 in the list: " + rndWords.Contains(two));
+            Console.WriteLine("\n");
+            //DeQueue's the first one in the list;
+            Console.WriteLine("{0}", rndWords.Dequeue());
+
+            //Looks at the next item in line after the one that has been Dequeued;
+            Console.WriteLine("{0}", rndWords.Peek());
+
+            //DeQueue's the first one in the list;
+            Console.WriteLine("{0}", rndWords.Dequeue());
+
+            //Looks at the next item in line after the one that has been Dequeued;
+            Console.WriteLine("{0}", rndWords.Peek());
+
+            //DeQueue's the first one in the list;
+            Console.WriteLine("{0}", rndWords.Dequeue());
+
+            Console.WriteLine();
+            // Print the total Queue's in the list
+            rndWords.GetAllQueueItems();
+
+            // Check for if the items are still in the Queue or not
+            Console.WriteLine("\n\nCheck for number 4 in the list: ");
+            rndWords.Contains(four);
+            Console.WriteLine("\n\nCheck for number 5 in the list: ");
+            rndWords.Contains(five);
+            Console.WriteLine("\n\nCheck for number 2 in the list: ");
+            rndWords.Contains(two);
 
             // End timer + results
             timer.Stop();
-            Console.WriteLine("\nTime needed for Queue: " + timer.Duration(TimeResolution.Seconds));
+            Console.WriteLine("\n\nTime needed for Queue: " + timer.Duration(TimeResolution.Seconds));
 
-            // Clear the whole Queue
-            Console.WriteLine("Clearing the Queue list!");
+            // Clears the whole Queue
             rndWords.Clear();
 
-            Console.WriteLine("Total Queue's left in the list: " + rndWords.Count() + " item");
+            // Remaing Queue in the list
+            rndWords.Count();
 
             Console.ReadLine();
-        }
-
-        public static void PrintValues(IEnumerable rndWords)
-        {
-            Console.WriteLine("\nItems in the Queue!");
-            foreach (object rndWord in rndWords)
-            {
-                Console.Write(rndWord + ", ");
-            }
         }
         #endregion
     }
